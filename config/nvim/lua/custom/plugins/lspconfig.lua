40a9a7f65dc6843ff2bdde1f8fa48305583463de
Bl0xhead <Bl0xhead@protonmail.com>
2022-04-11 23:57:52 +0100

    solc

--- config/nvim/lua/custom/plugins/lspconfig.lua
+++ config/nvim/lua/custom/plugins/lspconfig.lua
@@ -4,7 +4,7 @@ M.setup_lsp = function(attach, capabilities)
    local lspconfig = require "lspconfig"
 
    -- lspservers with default config
-   local servers = { "html", "cssls", "clangd", "solc" }
+   local servers = { "pyright","solidity_ls","html", "cssls", "solang","clangd", "solc" }
 
    for _, lsp in ipairs(servers) do
       lspconfig[lsp].setup {
