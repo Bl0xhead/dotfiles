d20c4ce85f2534ac9f236178d4b107ed6d69eb80
Bl0xhead <Bl0xhead@protonmail.com>
2022-05-02 00:00:15 +0100

    NVIM Config

---
+++ custom/plugins/configs.lua
@@ -0,0 +1,38 @@
+-- overriding default plugin configs!
+
+local M = {}
+
+M.treesitter = {
+   ensure_installed = {
+      "vim",
+      "html",
+      "css",
+      "javascript",
+      "solidity",
+      "typescript",
+      "rust",
+      "json",
+      "toml",
+      "markdown",
+      "c",
+      "bash",
+      "lua",
+   },
+}
+
+M.nvimtree = {
+   git = {
+      enable = true,
+   },
+}
+
+M.telescope = {
+   extensions = {
+     -- fd is needed
+      media_files = {
+         filetypes = { "png", "webp", "jpg", "jpeg" },
+      },
+   },
+}
+
+return M
